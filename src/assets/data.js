31aea3e4fdb85eaeec0fdfd1d45d058e4c5abb1b
Wiktor Walczak <w.walczak93@gmail.com>
2020-01-26 00:24:26 +0100

    Sponsors component refactor

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,3 +1,67 @@
+import mosir_sandomierz from '../images/sponsors/mosir_sandomierz.jpg';
+import mosir_tarnobrzeg from '../images/sponsors/mosir_tbg.JPG';
+import dom_dziecka from '../images/sponsors/domdzieckawskopaniu.jpg';
+import bbl from '../images/sponsors/biegambolubie.jpg';
+import radio_leliwa from '../images/sponsors/radioleliwa.png';
+import itv_wisla from '../images/sponsors/itv_wisla.png';
+import nadwisla24 from '../images/sponsors/nadwisla24.png';
+import echodnia from '../images/sponsors/echodnia.jpg';
+import sandomierztv from '../images/sponsors/sandomierztv.png';
+
+export const partners = [
+  {
+    link: 'http://mosir.sandomierz.pl/',
+    img: mosir_sandomierz,
+    alt: 'MOSIR Sandomierz'
+  },
+  {
+    link: 'http://mosir.tarnobrzeg.pl/',
+    img: mosir_tarnobrzeg,
+    alt: 'MOSIR Tarnobrzeg'
+  },
+  {
+    link: 'https://www.facebook.com/domdziecka.skopanie/',
+    img: dom_dziecka,
+    alt: 'Dom dziecka w Skopaniu'
+  },
+  {
+    link:
+      'https://www.facebook.com/pages/category/Community/Biegam-Bo-Lubi%C4%99-Sandomierz-1898374857096842/',
+    img: bbl,
+    alt: 'Biegam Bo Lubię'
+  }
+];
+
+export const mediaPatrones = [
+  {
+    link: 'http://leliwa.pl/',
+    img: radio_leliwa,
+    alt: 'Radio Leliwa'
+  },
+  {
+    link: 'https://itvwisla.tv/',
+    img: itv_wisla,
+    alt: 'iTV WISŁA'
+  },
+  {
+    link: 'http://nadwisla24.pl/',
+    img: nadwisla24,
+    alt: 'Nad Wisłą 24'
+  },
+  {
+    link: 'https://echodnia.eu/',
+    img: echodnia,
+    alt: 'Echo Dnia'
+  },
+  {
+    link: 'https://www.sandomierztv.pl/',
+    img: sandomierztv,
+    alt: 'Sandomierz tv'
+  }
+];
+
+export const currentYear = new Date().getUTCFullYear();
+
 export const routes = [
   {name: 'Start', path: '/'},
   {name: 'Wyniki', path: './result'},
@@ -7,5 +71,3 @@ export const routes = [
   {name: 'Regulamin', path: '/terms-and-conditions'},
   {name: 'Kontakt', path: '/contact'}
 ];
-
-export const currentYear = new Date().getUTCFullYear();
--- src/components/sponsors/Sponsors.js
+++ src/components/sponsors/Sponsors.js
@@ -2,16 +2,8 @@
 import React from 'react';
 
 import styles from './Sponsors.module.scss';
+import {partners, mediaPatrones} from '../../assets/data';
 import nsg from '../../images/sponsors/NSG.png';
-import mosir_sandomierz from '../../images/sponsors/mosir_sandomierz.jpg';
-import mosir_tarnobrzeg from '../../images/sponsors/mosir_tbg.JPG';
-import dom_dziecka from '../../images/sponsors/domdzieckawskopaniu.jpg';
-import bbl from '../../images/sponsors/biegambolubie.jpg';
-import radio_leliwa from '../../images/sponsors/radioleliwa.png';
-import itv_wisla from '../../images/sponsors/itv_wisla.png';
-import nadwisla24 from '../../images/sponsors/nadwisla24.png';
-import echodnia from '../../images/sponsors/echodnia.jpg';
-import sandomierztv from '../../images/sponsors/sandomierztv.png';
 
 const Sponsors = () => {
   return (
@@ -25,40 +17,20 @@ const Sponsors = () => {
 
       <h2>Partnerzy</h2>
       <div className={styles.partners}>
-        <a href="http://mosir.sandomierz.pl/" target="_blank">
-          <img src={mosir_sandomierz} alt="MOSIR Sandomierz" />
-        </a>
-        <a href="http://mosir.tarnobrzeg.pl/" target="_blank">
-          <img src={mosir_tarnobrzeg} alt="MOSIR Tarnobrzeg" />
-        </a>
-        <a href="https://www.facebook.com/domdziecka.skopanie/" target="_blank">
-          <img src={dom_dziecka} alt="Dom dziecka w Skopaniu" />
-        </a>
-        <a
-          href="https://www.facebook.com/pages/category/Community/Biegam-Bo-Lubi%C4%99-Sandomierz-1898374857096842/"
-          target="_blank"
-        >
-          <img src={bbl} alt="Biegam Bo Lubię" />
-        </a>
+        {partners.map((partner, i) => (
+          <a key={i} href={partner.link} target="_blank">
+            <img src={partner.img} alt={partner.alt} />
+          </a>
+        ))}
       </div>
 
       <h2>Patronat medialny</h2>
       <div className={styles.media_patron}>
-        <a href="http://leliwa.pl/" target="_blank">
-          <img src={radio_leliwa} alt="Radio Leliwa" />
-        </a>
-        <a href="https://itvwisla.tv/" target="_blank">
-          <img src={itv_wisla} alt="iTV WISŁA" />
-        </a>
-        <a href="http://nadwisla24.pl/" target="_blank">
-          <img src={nadwisla24} alt="Nad Wisłą 24" />
-        </a>
-        <a href="https://echodnia.eu/" target="_blank">
-          <img src={echodnia} alt="Echo Dnia" />
-        </a>
-        <a href="https://www.sandomierztv.pl/" target="_blank">
-          <img src={sandomierztv} alt="Sandomierz tv" />
-        </a>
+        {mediaPatrones.map((patrone, i) => (
+          <a key={i} href={patrone.link} target="_blank">
+            <img src={patrone.img} alt={patrone.alt} />
+          </a>
+        ))}
       </div>
     </section>
   );
