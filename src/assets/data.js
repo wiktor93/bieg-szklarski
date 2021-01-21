154779f1f855fca1f4e9ff5212de5938e2463fd1
Wiktor Walczak <w.walczak93@gmail.com>
2021-01-21 21:02:40 +0100

    Update banner details

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,12 +1,12 @@
-export const currentYear = new Date().getUTCFullYear();
-export const startDate = new Date('2021', '08', '19', '11'); //month is presented by numbers from 0 to 11
+export const currentYear = new Date().getUTCFullYear()
+export const startDate = new Date('2021', '08', '19', '11') //month is presented by numbers from 0 to 11
 
-export const registrationLink = 'https://timekeeper.pl/bieg/?bieg_id=841';
+export const registrationLink = 'https://timekeeper.pl/bieg/?bieg_id=841'
 
 export const contactDetails = {
   email: 'biegszklarski@gmail.com',
   phone: '+48 605 192 829',
-};
+}
 
 export const routes = [
   {name: 'Start', path: '/'},
@@ -16,10 +16,10 @@ export const routes = [
   {name: 'Trasa', path: '/trasa'},
   {name: 'Regulamin', path: '/regulamin'},
   {name: 'Kontakt', path: '/kontakt'},
-];
+]
 
 export const bannerDetails = [
-  {name: 'date', info: '19 września 2021', icon: 'fas fa-calendar-alt'},
+  {name: 'date', info: '26 września 2021', icon: 'fas fa-calendar-alt'},
   {
     name: 'localization',
     info: 'Tarnobrzeg — Sandomierz',
@@ -32,4 +32,4 @@ export const bannerDetails = [
   },
   {name: 'sub_event_2', info: 'Szklana Piątka — 5 km', icon: 'fas fa-running'},
   {name: 'sub_event_1', info: 'Nordic Walking — 5 km', icon: 'fas fa-walking'},
-];
+]
